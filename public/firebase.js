303ff4380bc2bac19a0e0bc87cd903cce78f0fed
Murillo <mumoura9@gmail.com>
2026-04-04 18:10:58 -0300

    index corrigido

--- public/firebase.js
+++ public/firebase.js
@@ -1,4 +1,4 @@
-// 🔥 IMPORTS FIREBASE
+// 🔥 IMPORTS
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 
 import {
@@ -11,9 +11,7 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 // 🔥 CONFIG (A SUA)
 const firebaseConfig = {
@@ -22,19 +20,17 @@ const firebaseConfig = {
   projectId: "debate-7904f",
   storageBucket: "debate-7904f.firebasestorage.app",
   messagingSenderId: "293851758581",
-  appId: "1:293851758581:web:07548e25f9e0dbd6422abc",
-  measurementId: "G-PZ4E0JL16D"
+  appId: "1:293851758581:web:07548e25f9e0dbd6422abc"
 };
 
-// 🔥 INICIALIZA
+// 🔥 INIT
 const app = initializeApp(firebaseConfig);
 
-// 🔥 SERVIÇOS
+// 🔥 EXPORTS
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 
-// 🔥 EXPORTA FUNÇÕES
 export {
   signInWithPopup,
   signInWithEmailAndPassword,
