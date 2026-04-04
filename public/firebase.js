642fc32cb9f4d265adddc7b6f6eb84613537337e
Murillo <mumoura9@gmail.com>
2026-04-04 18:35:15 -0300

    fix firebase db

--- public/firebase.js
+++ public/firebase.js
@@ -1,5 +1,5 @@
-// 🔥 IMPORTS
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+// 🔥 FIREBASE IMPORTS
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
   getAuth,
@@ -9,9 +9,11 @@ import {
   createUserWithEmailAndPassword,
   signOut,
   onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // 🔥 CONFIG (A SUA)
 const firebaseConfig = {
@@ -23,13 +25,13 @@ const firebaseConfig = {
   appId: "1:293851758581:web:07548e25f9e0dbd6422abc"
 };
 
-// 🔥 INIT
+// 🔥 INICIALIZA
 const app = initializeApp(firebaseConfig);
 
-// 🔥 EXPORTS
+// 🔥 EXPORTA CERTO
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
 
 export {
   signInWithPopup,
